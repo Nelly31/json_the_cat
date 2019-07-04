258449a7241008147ede5b118e63d087d04eea10
Nelly31 <nelly_main@hotmail.com>
2019-07-04 19:17:06 +0000

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,20 +3,33 @@ const request = require('request');
 // https://api.thecatapi.com/v1/breeds/search
 // https://api.thecatapi.com/v1/breeds/search?q=CATNAME
 
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
 
-const breedFetcher = () => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+// original code
+// const breedFetcher = () => {
+//   request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+//     const data = JSON.parse(body);
+//     if (!data.length) {
+//       console.log('Sorry! That cat breed isn\'t found');
+//     } else if (data.message === "Not Found") {
+//       console.log(`Error ${data.status}`);
+//     } else {
+//       console.log(data[0].description);
+//     }
+//   });
+// };
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
-    if (!data.length) {
-      console.log('Sorry! That cat breed isn\'t found');
-    } else if (data.message === "Not Found") {
-      console.log(`Error ${data.status}`);
+    if (!data.length || data.message === "Not Found") {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
   });
 };
 
-breedFetcher();
 
+// module.exports = breedFetcher;
+module.exports = {fetchBreedDescription};
